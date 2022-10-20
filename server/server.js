daa39d2f3bd4b61cde0bd53f8e2c8ef01514f395
cduwors <christinaduwors@gmail.com>
2022-10-20 16:27:17 -0400

    concurrently and server set up

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
+const path = require('path');
 
 const db = require("./config/connection");
 const { typeDefs, resolvers } = require("./schemas");
@@ -17,6 +18,16 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  
+  app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
+ // Create a new instance of an Apollo server with the GraphQL schema 
 const startApolloServer = async (typeDefs, resolvers) => {
 	await server.start();
 	server.applyMiddleware({ app });
@@ -30,5 +41,5 @@ const startApolloServer = async (typeDefs, resolvers) => {
 		});
 	});
 };
-
+  // Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
